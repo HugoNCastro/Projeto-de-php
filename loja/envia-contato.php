1bd6117dbf1a2181db359d47f01c39fdb1c4ce37
Unknown <hugonaaman@gmail.com>
2018-05-18 08:38:27 -0300

    att envia contato

--- loja/envia-contato.php
+++ loja/envia-contato.php
@@ -15,30 +15,31 @@ $mail->isSMTP();
 $mail->Host = 'smtp.gmail.com';
 $mail->Port = 587;
 $mail->SMTPSecure = 'tls';
-$mail->STMPAuth = true;
-$mail->Username = "alura.php.e.mysql@gmail.com";
-$mail->Password = "123456"
+$mail->SMTPAuth = true;
+$mail->Username = "hugonaaman@gmail.com";
+$mail->Password = "123456";
 
 //Dados do meu email, quem esta enviando o email
-$mail->setFrom("alura.php.e.mysql@gmail.com", "Alura Curso PHP e MySQL");
+$mail->setFrom("hugonaaman@gmail.com", "Alura Curso PHP e MySQL");
+
 
 //endereço alvo quem vai receber o email
 $mail->addAddress("hugonaaman@gmail.com");
-$mail->Subject = "Email de contato de loja";
+$mail->Subject = "Email de contato da loja";
+
 
 //Conteúdo que será enviado no email
-$mail->msgHTML("<html>de: {$nome}<br/>email: {$email}<br/>mensagem {$mensagem}</html>");
-$mail->AltBody = "de: {$nome}\nemail: {$email}\nmensagem {$mensagem}";
+$mail->msgHTML("<html>de: {$nome}<br/>email: {$email}<br/>mensagem: {$mensagem}</html>");
+$mail->AltBody = "de: {$nome}\nemail:{$email}\nmensagem: {$mensagem}";
 
-if ($mail->send()) {
-	$_SESSION['success'] = "Mensagem enviada com êxito.";
-	header("Location: index.php");
-}else{
-	$_SESSION['danger'] = "Erro ao enviar a mensagem, verifique os dados e tente novamente." . $mail->ErrorInfo;
-	header("Location: contato.php");
+if($mail->send()) {
+    $_SESSION["success"] = "Mensagem enviada com sucesso";
+    header("Location: index.php");
+} else {
+    $_SESSION["danger"] = "Erro ao enviar mensagem " . $mail->ErrorInfo;
+    header("Location: contato.php");
 }
 die();
-
 
 
 
